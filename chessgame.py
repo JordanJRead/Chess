a0dc9e9e0f63e69199c141686a6e1f7e01d01809
Jordan Read <jordan.j.read@gmail.com>
2024-06-09 14:29:18 -0400

    Fixed a bug where a piece would not be able to be selected to move in some cases

--- chessgame.py
+++ chessgame.py
@@ -9,10 +9,9 @@ square_size = dimensions / 8
 image_scale_size = square_size + 1
 pygame.init()
 screen = pygame.display.set_mode((dimensions, dimensions))
-
 game = chessengine.GameState()
 
-def draw_square(square):
+def draw_valid_square(square):
     coords = square_to_xy(square)
     screen.blit(valid_move_image, coords)
     pygame.display.update()
@@ -81,7 +80,7 @@ def promote_select():
         screen.blit(bR_image, (square_to_xy(37)))
         screen.blit(bB_image, (square_to_xy(28)))
         screen.blit(bN_image, (square_to_xy(29)))
-    
+   
     pygame.display.update()
     selecting = True
     while selecting:
@@ -93,7 +92,7 @@ def promote_select():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Get what square the mouse clicked on
                 selected_square = xy_to_square(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-                
+               
                 if selected_square == 36:
                     return "Q"
                 elif selected_square == 37:
@@ -105,33 +104,32 @@ def promote_select():
 
 valid_move_image = pygame.transform.scale(pygame.image.load("data/valid_move.png"), (image_scale_size, image_scale_size))
 valid_move_image.set_alpha(100)
-board_image = pygame.transform.scale(pygame.image.load("data\chess_board.png"), (dimensions, dimensions))
-
+board_image = pygame.transform.scale(pygame.image.load("data/chess_board.png"), (dimensions, dimensions))
 checkmate_image = pygame.transform.scale(pygame.image.load("data/checkmate_square.svg"), (image_scale_size, image_scale_size))
-checkmate_image.set_alpha(200)
+#checkmate_image.set_alpha(255)
 
-wP_image = pygame.transform.scale(pygame.image.load("data\white_pawn.svg"), (image_scale_size, image_scale_size))
-wR_image = pygame.transform.scale(pygame.image.load("data\white_rook.svg"), (image_scale_size, image_scale_size))
-wN_image = pygame.transform.scale(pygame.image.load("data\white_knight.svg"), (image_scale_size, image_scale_size))
-wB_image = pygame.transform.scale(pygame.image.load("data\white_bishop.svg"), (image_scale_size, image_scale_size))
-wQ_image = pygame.transform.scale(pygame.image.load("data\white_queen.svg"), (image_scale_size, image_scale_size))
-wÑ_image = pygame.transform.scale(pygame.image.load("pieces_vector\white_knook_better.xcf"), (image_scale_size, image_scale_size))
-wRight_image = pygame.transform.scale(pygame.image.load("pieces_vector\white_right.xcf"), (image_scale_size, image_scale_size))
+wP_image = pygame.transform.scale(pygame.image.load("data/white_pawn.svg"), (image_scale_size, image_scale_size))
+wR_image = pygame.transform.scale(pygame.image.load("data/white_rook.svg"), (image_scale_size, image_scale_size))
+wN_image = pygame.transform.scale(pygame.image.load("data/white_knight.svg"), (image_scale_size, image_scale_size))
+wB_image = pygame.transform.scale(pygame.image.load("data/white_bishop.svg"), (image_scale_size, image_scale_size))
+wQ_image = pygame.transform.scale(pygame.image.load("data/white_queen.svg"), (image_scale_size, image_scale_size))
+wÑ_image = pygame.transform.scale(pygame.image.load("pieces_vector/white_knook_better.xcf"), (image_scale_size, image_scale_size))
+wRight_image = pygame.transform.scale(pygame.image.load("pieces_vector/white_right.xcf"), (image_scale_size, image_scale_size))
 
-wK_image = pygame.transform.scale(pygame.image.load("data\white_king.svg"), (image_scale_size, image_scale_size))
+wK_image = pygame.transform.scale(pygame.image.load("data/white_king.svg"), (image_scale_size, image_scale_size))
 bP_image = pygame.transform.scale(pygame.image.load("data/black_pawn.svg"), (image_scale_size, image_scale_size))
 bR_image = pygame.transform.scale(pygame.image.load("data/black_rook.svg"), (image_scale_size, image_scale_size))
 bN_image = pygame.transform.scale(pygame.image.load("data/black_knight.svg"), (image_scale_size, image_scale_size))
 bB_image = pygame.transform.scale(pygame.image.load("data/black_bishop.svg"), (image_scale_size, image_scale_size))
 bQ_image = pygame.transform.scale(pygame.image.load("data/black_queen.svg"), (image_scale_size, image_scale_size))
 
-wR_image_alpha = pygame.transform.scale(pygame.image.load("data\white_rook.svg"), (image_scale_size, image_scale_size))
+wR_image_alpha = pygame.transform.scale(pygame.image.load("data/white_rook.svg"), (image_scale_size, image_scale_size))
 wR_image_alpha.set_alpha(100)
-wN_image_alpha = pygame.transform.scale(pygame.image.load("data\white_knight.svg"), (image_scale_size, image_scale_size))
+wN_image_alpha = pygame.transform.scale(pygame.image.load("data/white_knight.svg"), (image_scale_size, image_scale_size))
 wN_image_alpha.set_alpha(100)
-wB_image_alpha = pygame.transform.scale(pygame.image.load("data\white_bishop.svg"), (image_scale_size, image_scale_size))
+wB_image_alpha = pygame.transform.scale(pygame.image.load("data/white_bishop.svg"), (image_scale_size, image_scale_size))
 wB_image_alpha.set_alpha(100)
-wQ_image_alpha = pygame.transform.scale(pygame.image.load("data\white_queen.svg"), (image_scale_size, image_scale_size))
+wQ_image_alpha = pygame.transform.scale(pygame.image.load("data/white_queen.svg"), (image_scale_size, image_scale_size))
 wQ_image_alpha.set_alpha(100)
 
 bR_image_alpha = pygame.transform.scale(pygame.image.load("data/black_rook.svg"), (image_scale_size, image_scale_size))
@@ -150,24 +148,24 @@ selected_a_piece = False
 draw_board()
 valid_moves = []
 while game_on: # Game loop
-    
+   
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_on = False
         # Selecting a square
         if event.type == pygame.MOUSEBUTTONDOWN:
-            
+           
             # Get what square the mouse clicked on
             selected_square = xy_to_square(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-            if game.board[selected_square][0] == game.turn and selected_square not in valid_moves: # Chose a correct piece
+            if game.board[selected_square][0] == game.turn and selected_square not in valid_moves: # Clicking on a valid square would just move there
                 selected_piece = selected_square
                 selected_a_piece = True
                 valid_moves = game.find_moves(selected_piece)
                 draw_board()
                 if bool(valid_moves):
                     for i in valid_moves:
-                        draw_square(i)
+                        draw_valid_square(i)
 
             elif selected_a_piece: # Move the piece
                 if selected_square in valid_moves:
@@ -180,6 +178,7 @@ while game_on: # Game loop
                         game.promote(destination, promote_to)
 
                     game.end_turn()
+                    valid_moves = []
                     selected_a_piece = False
                     draw_board()
                     print(game.check_end_condition(game.turn))
@@ -194,4 +193,7 @@ while game_on: # Game loop
                         screen.blit(checkmate_image, square_to_xy(king_square))
                         valid_moves = []
                         pygame.display.update()
+                   
+                    # Stalemate
+                   
 pygame.quit()
